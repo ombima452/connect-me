7d446a4be7fd77ab716507e0a74fc106138c8f01
alex-muliande <amuliande@gmail.com>
2019-09-26 15:39:52 +0300

    Add flask import uploads to the init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -12,6 +13,7 @@ login_manager.login_view = 'auth.login'
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -21,6 +23,12 @@ def create_app(config_name):
     db.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
+
+
+
+      # configure UploadSet
+    configure_uploads(app,photos)
+
 
     return app
 
