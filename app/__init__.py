93a9737856cc4bd65e28f28a862fce21d4875fb7
Wachira Beatrice <BeatriceWambui>
2019-09-26 10:13:06 +0300

    create url for main.index that redirects user to index after they log in

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,3 @@
-
-
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
@@ -18,6 +16,8 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
     from  .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
     db.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,12 @@
 from . import db 
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class User(UserMixin,db.Model):
     __tablename__='users'
@@ -54,7 +60,7 @@ class Comments(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     comment=db.Column(db.String(255))
     name=db.Column(db.String(255))
-    number=db.Column(db.Integer(255))
+    # number=db.Column(db.Integer(255))
     user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
     post_id=db.Column(db.Integer,db.ForeignKey('posts.id'))
     def save(self):
