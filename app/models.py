53303da0c452f1175a75a3b8e08a808b45bf127a
alex-muliande <amuliande@gmail.com>
2019-09-27 11:14:31 +0300

    Add user loader

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db 
+from app import login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 
@@ -14,6 +15,13 @@ class User(UserMixin,db.Model):
     email=db.Column(db.String(255),unique = True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+
+
+    @login_manager.user_loader
+    def get_user(user_id):
+        return User.query.get(user_id)
+
+
 
     @property
     def password(self):
